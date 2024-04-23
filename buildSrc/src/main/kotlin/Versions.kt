4fbec4c39ec6d1cb53c3927364703adc0c15b068
Guillaume Muret <guillaume.muret.pro@gmail.com>
2024-04-23 09:17:38 +0200

    feat(#10): Remove Gson dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,6 @@ object Versions {
     const val appcompat = "1.6.1"
     const val coreKtx = "1.12.0"
     const val ktor = "2.3.6"
-    const val gson = "2.10.1"
     const val buildkonfigGradlePlugin = "0.13.3"
     const val activityCompose = "1.8.0"
     const val firebaseMessaging = "23.3.1"
